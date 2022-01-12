42fff363c84a9dc9d375e2650e0c35dbf5d89b38
yusufcr <yaslan@credly.com>
2022-01-12 02:00:55 -0600

    First Commit

--- src/test/java/cbt/Driver.java
+++ src/test/java/cbt/Driver.java
@@ -35,7 +35,7 @@ public class Driver {
                 case "remote-chrome":
                     try {
                         // assign your grid server address
-                        String gridAddress = "18.206.56.199";
+                        String gridAddress = "54.157.119.62";
                         URL url = new URL("http://" + gridAddress + ":4444/wd/hub");
                         DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
                         desiredCapabilities.setBrowserName("chrome");
