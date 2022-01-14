c2de356337c381de42519246d7b0e58ab49e526d
yusufcr <yaslan@credly.com>
2022-01-13 18:02:55 -0600

    First Commit

--- src/test/java/cbt/CukesRunner.java
+++ src/test/java/cbt/CukesRunner.java
@@ -7,10 +7,11 @@ import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
 @CucumberOptions(
         plugin = {
+                "pretty",
                 "json:target/cucumber.json",
                 "html:target/cucumber/report.html",
                 "junit:target/junit/junit-report.xml",
-                "rerun:target/rerun.txt",},
+                "rerun:target/rerun.txt"},
         features = "src/test/resources/cbt",
         publish = true
 )
